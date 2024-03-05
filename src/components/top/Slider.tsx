cd209582f0b83a68e7780803d923840c7212f0ef
shimizuyasushi <51806272+kasuuuu4423@users.noreply.github.com>
2024-03-06 04:22:23 +0900

    ページネーション追加

--- src/components/top/Slider.tsx
+++ src/components/top/Slider.tsx
@@ -2,20 +2,29 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import Navigation from 'swiper';
-import AutoPlay from 'swiper';
-import Pagination from 'swiper';
+import { Pagination } from 'swiper/modules';
 
 import { type Work } from '../../lib/WP';
 
 const Slider = (props: {works: Work[]}) =>{
 
     return(
-        <section className='w-full'>
+        <section className='
+            w-full
+            [&_.swiper-pagination-bullet]:bg-white
+            [&_.swiper-pagination-bullet]:w-3
+            [&_.swiper-pagination-bullet]:h-3
+            [&_.swiper-pagination-bullet]:!mx-2
+            '>
             <Swiper
                 className='w-full md:h-full h-96'
                 slidesPerView={1}
                 loop={true}
+                modules={[Pagination]}
+                pagination={{
+                    "enabled": true,
+                    "clickable": true,
+                }}
             >
                 {props.works.map(work=>
                     <SwiperSlide className='w-full' key={work.id}>
