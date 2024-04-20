d13ef7a5c53a46c85e839fccf3363a62d42fd6ef
YasushiShimizu <decode4423@gmail.com>
2024-04-21 05:15:41 +0900

    アナリティクスとサイトマップ追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,28 @@ import react from "@astrojs/react";
 //import nodejs from '@astrojs/node';
 
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   //output: "hybrid",
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   image: {
     domains: ["smitharia.shimizuyasushi.com"]
-  },
+  }
   // adapter: node({
   //   mode: "standalone"
   // })
